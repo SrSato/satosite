2225be25418bb8ef7aadf85de73ff7e2145d4809
SrSato <38430735+SrSato@users.noreply.github.com>
2021-08-16 20:47:17 +0200

    Add files via upload
    Saved for historical reviewers :P

---
+++ satosite.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	
+	
+  	$(".desplegar").click(function(){
+	  	
+  		$(".art_main").removeClass("desplegado");	
+	  	$(this).parent().addClass("desplegado");
+	});  	
+	
+});
